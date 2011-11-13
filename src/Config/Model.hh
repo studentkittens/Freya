58341b414958a6b02bfb68ce3c4caa86eb8683b7
dieKatze <christoph.piechula@hof-university.de>
2011-11-13 15:11:56 +0100

    defaultdoc changes.

--- src/Config/Model.hh
+++ src/Config/Model.hh
@@ -2,7 +2,6 @@
 #define FREYA_CONFIG_MODEL_ATB3SUAE
 
 #include <libxml/parser.h>
-
 #include "./defaultcfg.inl"
 
 #define outputfile "./config.xml"
@@ -17,20 +16,22 @@ namespace Config
 
             /*loader and saver funcs for xml file*/
             void load(char*);
+            void loadDefaultDoc();
             void save();
             /*save as different filename eg. config.xml.bak*/
             void save(char*, xmlDocPtr);
 
             /*xml document getter*/
             xmlDocPtr getDocPtr();
+            xmlDocPtr getDefaultDocPtr();
 
         private:
-            /*xml document and xml node member*/
-            xmlDocPtr fileDoc;
-            xmlDocPtr defaultDoc;
-            xmlNodePtr curNode;
-            void setpath(char*);
-            char* pathtofile;
+                /*xml document and xml node member*/
+                xmlDocPtr fileDoc;
+                xmlDocPtr defaultDoc;
+                xmlNodePtr curNode;
+                void setpath(char*);
+                char* pathtofile;
     };
 }
 

13656ff9ea2e20e6f4aa8f43a9f9ab1299380aed
Chris P <sahib@online.de>
2011-11-29 23:52:32 +0100

    Some merging.

--- src/Browser/DatabasePopup.cc
+++ src/Browser/DatabasePopup.cc
@@ -16,7 +16,7 @@ namespace Browser
 
     DatabasePopup::DatabasePopup(Gtk::Widget& to_register) : BasePopup(to_register,ui_info)
     {
-        menu_add_item(m_ActionAdd,"db_add","Add Playlist","add to queue",Gtk::Stock::ADD);
+        menu_add_item(m_ActionAdd,"db_add","Add","Add to Queue",Gtk::Stock::ADD);
         menu_add_item(m_ActionAddToPL,"db_add_to_playlist","Add to","Add to a certain playlist",Gtk::Stock::GOTO_LAST);
         menu_add_item(m_ActionReplace,"db_replace","Replace","remove selected playlists",Gtk::Stock::JUMP_TO);
         menu_add_item(m_ActionRefresh,"db_refresh","Refresh","remove selected playlists",Gtk::Stock::REFRESH);

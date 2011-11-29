a16caf841de229230f2fc67457f4583c76688bde
eddY <eschneider@hof-university.de>
2011-11-29 01:39:01 +0100

    added wrapper function for ALLE kindo f combinations of char* and ustring...

--- src/Notify/Notify.cc
+++ src/Notify/Notify.cc
@@ -18,7 +18,7 @@ namespace Notify
         clear_icon();
     }
     //---------------------------
-    /* Wrapper functions */
+    /* Wrapper functions ... phiu I'm cool for providing all possible combinations of char* and ustring. Right? */
     //---------------------------
     void Notify::send(Glib::ustring &msg)
     {
@@ -51,6 +51,16 @@ namespace Notify
         _send(hl,msg,NULL);
     }
     //---------------------------
+    void Notify::send_big(Glib::ustring &hl, const char *msg)
+    {
+        _send(hl.c_str(),msg,NULL);
+    }
+    //---------------------------
+    void Notify::send_big(const char *hl, Glib::ustring &msg)
+    {
+        _send(hl,msg.c_str(),NULL);
+    }
+    //---------------------------
     void Notify::send_pic(const char *msg, GdkPixbuf * pixbuf)
     {
         _send(NULL,msg,pixbuf);
@@ -59,6 +69,16 @@ namespace Notify
     void Notify::send_full(const char *hl, const char *msg, GdkPixbuf * pixbuf )
     {
         _send(hl,msg,pixbuf);
+    }
+    //---------------------------
+    void Notify::send_full(Glib::ustring &hl, const char *msg, GdkPixbuf * pixbuf )
+    {
+        _send(hl.c_str(),msg,pixbuf);
+    }
+    //---------------------------
+    void Notify::send_full(const char *hl, Glib::ustring &msg, GdkPixbuf * pixbuf )
+    {
+        _send(hl,msg.c_str(),pixbuf);
     }
     //---------------------------
     /**/
--- src/Notify/Notify.hh
+++ src/Notify/Notify.hh
@@ -25,8 +25,12 @@ namespace Notify
             void send_full(Glib::ustring &hl, Glib::ustring &msg, GdkPixbuf * pixbuf);
             void send(const char *msg);
             void send_big(const char *hl, const char *msg);
+            void send_big(Glib::ustring &hl, const char *msg);
+            void send_big(const char *hl, Glib::ustring &msg);
             void send_pic(const char *msg, GdkPixbuf * pixbuf);
             void send_full(const char *hl,const char *msg, GdkPixbuf * pixbuf);
+            void send_full(Glib::ustring &hl, const char *msg, GdkPixbuf * pixbuf);
+            void send_full(const char *hl, Glib::ustring &msg, GdkPixbuf * pixbuf);
             void set_stock_icon(const char*);
         private:
             Glib::ustring * icon;

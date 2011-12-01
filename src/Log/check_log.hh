3addceae1f5df0d1a75d4984fef18b0d35892142
Chris P <sahib@online.de>
2011-12-01 19:49:41 +0100

    Added cmake integration for cxxtest. Run make test

---
+++ src/Log/check_log.hh
@@ -0,0 +1,18 @@
+/*  Sample test */
+#include <cxxtest/TestSuite.h>
+#include "Writer.hh"
+
+/* The whole suite. One suit for each module. */
+class LogTestSuite : public CxxTest::TestSuite 
+{
+    public:
+        /* A testcase == a method */
+        void testAddition( void )
+        {
+            TS_ASSERT( 1 + 1 > 1 );
+            TS_ASSERT_EQUALS( 1 + 1, 2 );
+
+            /* Test if linking works */
+            Log::Writer::instance();
+        }
+};

8b110ce0545ace63365deb50810a29e45fbb2777
dieKatze <christoph.piechula@hof-university.de>
2011-12-03 19:19:29 +0100

    notidy/log test files added

---
+++ src/Notify/check_notify.hh
@@ -0,0 +1,32 @@
+/*  Sample test */
+#include <cxxtest/TestSuite.h>
+#include "Notify.hh"
+
+class NotifyTestSuite : public CxxTest::TestSuite {
+
+    public:
+
+        void testsend_big( void )
+        {
+            TS_ASSERT(0==0);
+        }
+
+        void testsend_full( void )
+        {
+            TS_ASSERT(0==0);
+        }
+
+        void testset_stock_icon( void )
+        {
+            TS_ASSERT(0==0);
+        }
+
+        void testset_next_extra( void )
+        {
+            TS_ASSERT(0==0);
+        }
+
+        
+    private:
+        /* data */
+};
